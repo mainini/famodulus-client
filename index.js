1f400f270ba8e21972069111b32659bbce7166f2
Pascal Mainini <pascal@mainini.ch>
2016-11-22 23:55:07 +0100

    initial implementation of remote modexp

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
-var bigInt = require("big-integer");
+var bigInt = require('big-integer');
 
-var Famodulus = (function (undefined) {
-    "use strict";
-})();
+function Famodulus(servers) {
+    this.servers = servers;
+}
+
+Famodulus.prototype.modexp = function (base, exponent, modulus, callback) {
+    var query = {'modexps': [{'b': base, 'e': exponent, 'm': modulus}]};
+
+    var req = new XMLHttpRequest();
+    req.addEventListener('load', function () {
+        var res = JSON.parse(this.responseText);
+        callback(res.modexps[0].r);
+    });
+
+    req.open('POST', this.servers[0]);
+    req.setRequestHeader("Content-type", "application/json");
+    req.send(JSON.stringify(query));
+};
 
 if (typeof window !== 'undefined') {
     window.bigInt = bigInt;
     window.Famodulus = Famodulus;
 }
 
-if (typeof module !== "undefined" && module.hasOwnProperty("exports")) {
+if (typeof module !== 'undefined' && module.hasOwnProperty('exports')) {
     module.exports = Famodulus;
 }

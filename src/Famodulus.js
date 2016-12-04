fecfb37b9dad7c27ff1aae2d5c1b64ee71797f80
Pascal Mainini <pascal@mainini.ch>
2016-12-04 17:15:02 +0100

    jshinting

--- src/Famodulus.js
+++ src/Famodulus.js
@@ -1,10 +1,13 @@
-module.exports = FamodulusClient;
+/*jshint node:true, eqeqeq:true, esversion:5, bitwise:true, curly:true, immed:true, indent:4, latedef:true, newcap:true, noarg:true, noempty:true, nonew:true, quotmark:single, undef:true, unused:true, trailing:true, white:false, forin:true, futurehostile:true, nonbsp:true, nonew:true, strict:true */
+/* globals XMLHttpRequest */
 
 function FamodulusClient(servers) {
+    'use strict';
     this.servers = servers;
 }
 
 FamodulusClient.prototype.modexp = function (base, exponent, modulus, callback) {
+    'use strict';
     var query = {'modexps': [{'b': base, 'e': exponent, 'm': modulus}]};
 
     var req = new XMLHttpRequest();
@@ -14,6 +17,8 @@ FamodulusClient.prototype.modexp = function (base, exponent, modulus, callback) 
     });
 
     req.open('POST', this.servers[0]);
-    req.setRequestHeader("Content-type", "application/json");
+    req.setRequestHeader('Content-type", "application/json');
     req.send(JSON.stringify(query));
 };
+
+module.exports = FamodulusClient;
--- src/bigint.js
+++ src/bigint.js
@@ -1,29 +1,34 @@
-if (typeof verificatum !== 'undefined') {
-    console.log("Verificatum library detected!");
-    module.exports = {
-        'modexp':_modexpVerificatum
-    };
-} else {
-    var _BigInt = require('BigInt');
-
-    module.exports = {
-        'modexp':_modexpLeemon
-    };
-}
+/*jshint node:true, eqeqeq:true, esversion:5, bitwise:true, curly:true, immed:true, indent:4, latedef:true, newcap:true, noarg:true, noempty:true, nonew:true, quotmark:single, undef:true, unused:true, trailing:true, white:false, forin:true, futurehostile:true, nonbsp:true, nonew:true, strict:true */
+/* globals verificatum */
 
 function _modexpVerificatum(base, exponent, modulus)
 {
+    'use strict';
     var b = new verificatum.arithm.LargeInteger(base);
     var e = new verificatum.arithm.LargeInteger(exponent);
     var m = new verificatum.arithm.LargeInteger(modulus);
 
-    return b.modPow(e,m).toHexString(16)
+    return b.modPow(e,m).toHexString(16);
 }
 
 function _modexpLeemon(base, exponent, modulus) {
+    'use strict';
+    var _BigInt = require('BigInt');
+
     var b = _BigInt.str2bigInt(base,16,0);
     var e = _BigInt.str2bigInt(exponent,16,0);
     var m = _BigInt.str2bigInt(modulus,16,0);
 
     return _BigInt.bigInt2str(_BigInt.powMod(b,e,m),16);
+}
+
+if (typeof verificatum !== 'undefined') {
+    console.log('Verificatum library detected!');
+    module.exports = {
+        'modexp':_modexpVerificatum
+    };
+} else {
+    module.exports = {
+        'modexp':_modexpLeemon
+    };
 }

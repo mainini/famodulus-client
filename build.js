c0019963cc526d096aaf348816355f9535017a92
Pascal Mainini <pascal@mainini.ch>
2016-12-28 23:56:14 +0100

    generating jsdoc on build

--- build.js
+++ build.js
@@ -4,3 +4,4 @@ require('shelljs/global');
 
 mkdir('-p', '.build/js');
 exec('browserify lib/client.js --standalone FamodulusClient -o .build/js/famodulus.browser.js');
+exec('jsdoc -d .build/doc lib/*');

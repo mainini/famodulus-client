e82630cd01740794b0157f772aecf2ecba55d636
Pascal Mainini <pascal@mainini.ch>
2016-12-28 17:35:44 +0100

    implementation of direct

--- test/test.js
+++ test/test.js
@@ -1,11 +1,12 @@
 var test = require('tape');
-var famodulus = require('../');
+var FamodulusClient = require('../');
+
+var fam = new FamodulusClient(['http://localhost:8081/api/modexp/'], ['http://localhost:8081/api/modexp/']);
 
 test('direct', function (t) {
-    t.plan(1);
+  t.plan(1);
 
-    var f = new famodulus(['http://localhost:8081/api/modexp/']);
-    f.direct('2','4','b').then(result => {
-      t.equal(result.r, '5');
-    });
+  fam.direct('2', '4', 'b').then(result => {
+    t.equal(result.r, '5');
+  });
 });
